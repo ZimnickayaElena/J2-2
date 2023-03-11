b06963a619d3dd05c5737972fc2141e442445cda
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-03-11 17:11:07 +0400

    CommitTwo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int begining = 100;
+        int cash = 1300;
+        int bonus = cash / 100;
+
+        if (cash >= 1000) {
+            System.out.println("Вам начислено " + bonus + " баллов");
+            System.out.println("На вашем счете " + (begining + cash) + " рублей");
+        } else {
+            System.out.println("Вам начислено " + 0 + " баллов");
+            System.out.println("На вашем счете " + (begining + cash) + " рублей");
+        }
+
     }
 }
