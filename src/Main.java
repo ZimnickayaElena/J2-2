4bd24fa34f01a9e17d5ebf9e9c5d0f60d2ed4c5f
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-03-15 19:40:38 +0400

    CommitThree

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int begining = 100;
-        int cash = 1300;
-        int bonus = cash / 100;
+        int cash = 1500;
 
-        if (cash >= 1000) {
-            System.out.println("Вам начислено " + bonus + " баллов");
-            System.out.println("На вашем счете " + (begining + cash) + " рублей");
+        if (cash > 1000) {
+            int bonus = cash / 100;
+            System.out.println("На вашем счете " + (begining + cash) + " рублей и " + bonus + " бонусов");
         } else {
-            System.out.println("Вам начислено " + 0 + " баллов");
             System.out.println("На вашем счете " + (begining + cash) + " рублей");
         }
 
